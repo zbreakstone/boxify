31090d986091a068283e2e3a80e17e0214b0df1f
Zack Breakstone <z.breakstone@gmail.com>
2015-02-05 00:57:29 -0500

    Testing autoprefixer

--- js/script.js
+++ js/script.js
@@ -4,11 +4,3 @@ $(document).ready(function(){
 	});
 });
 
-// Experimental Slider Below
-// $(document).ready(function(){
-//   $('.slider-btns li').click(function(){
-//     $('.slider-content li').addClass('slide');
-//     // $(this).siblings().addClass('slide');
-//   });
-// });
-// End Experimental Slider
