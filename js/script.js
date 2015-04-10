be4e4b66d46f05d1a4510d5e75f786cee3b30231
Zack Breakstone <z.breakstone@gmail.com>
2015-04-09 23:55:48 -0400

    Add smoothscroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function(e){
 	$('.nav-link').click(function(){
 		$(this).toggleClass('open');
 		$('.overlay.menu-overlay').toggleClass('open');
+	});
+
+	$('.logo a, .overlay ul li > a, .bottom-nav ul > li > a').bind('click', function(e) {
+	    e.preventDefault();
+	    $('html,body').animate({scrollTop: $(this.hash).offset().top - 0}, 1000);                                                         
 	});
 });
 
